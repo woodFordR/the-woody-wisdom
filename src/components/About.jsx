5c3b3da0b2c7aaef8e798e63fa22f01ba876c7fa
woodFordR <adam@kaewell.com>
2024-12-18 22:45:20 -0500

    added react to astrojs, created react component About and nav links with Layout.astro

---
+++ src/components/About.jsx
@@ -0,0 +1,25 @@
+const About = () => {
+  return (
+    <div id="container">
+      <main>
+        <h1>about woodford</h1>
+        <section id="hero">
+          <hr />
+
+          <p>he is a chocolate lab</p>
+          <hr />
+          <h2>woody's hobbies</h2>
+          <ul>
+            <li>play fetch</li>
+            <li>go for walks</li>
+            <li>belly rubs</li>
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+
+export default About;
+
