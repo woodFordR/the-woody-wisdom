cd90960604131f811694da0c46313206fbc1127e
woodFordR <adam@kaewell.com>
2024-12-21 01:02:12 -0500

    adding component to index, defining blog component

---
+++ src/components/Blog.jsx
@@ -0,0 +1,19 @@
+const Blog = () => {
+  return (
+    <div id="container">
+      <main>
+        <section id="hero">
+          <hr />
+          <h2>woody's posts</h2>
+          <ul>
+            <li><a href="/posts/post-1">Post 1</a></li>
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+
+export default Blog;
+
